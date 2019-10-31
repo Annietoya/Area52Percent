7e94676bc0030e03ca75dab4182d6182a9fa4a63
Annietoya <ava48@drexel.edu>
2019-10-31 01:37:39 -0400

    Test for GDD. Ignore me!

---
+++ V1/CowDuction/Assets/Scripts/SC_CowSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+// Environment - Active Spawning
+// this script manages the spawning of cows and will be placed on a gameobject pertaining to the barns and other cow sources
+
+public class SC_CowSpawner : MonoBehaviour
+{
+    public GameObject cowPrefab;
+    public int cowAmount = 10;
+    public float radius = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Error checking
+        if (cowPrefab == null)
+            Debug.LogError("Cow prefab not assigned");
+        if (cowAmount < 1)
+            cowAmount = 10;
+        if (radius < 1)
+            radius = 10;
+
+        // Spawn cows        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ V1/CowDuction/Assets/Scripts/SC_SliderText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// UI - parameters screen
+// this script is applied to the menu object that combines each individual parameter slider and text
+
+public class SC_SliderText : MonoBehaviour
+{
+    public Slider _Slider;
+    public Text _Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Text.text = _Slider.value.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Text.text = _Slider.value.ToString();
+    }
+}
